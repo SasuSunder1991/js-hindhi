174143a06173eaf388ebe830c8b3a3f4b0b38cbb
SasuSunder1991 <sasu@qmspot.com>
2025-05-18 14:29:46 +0000

    Convertion of database

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = false
+// console.log(typeof score);
+// console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+console.log(valueInNumber)
+// Conversion 
+ // Number convertion is simple "33" => 33
+ // "33abc" => NaN Not a Number
+ // 
+ let isLoggedIn = 1
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
